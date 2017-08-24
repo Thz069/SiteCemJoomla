798471e41dd7d9a5b148f1050a84e6c6362080d7
Thz069 <victor.thoma@gmail.com>
2017-08-24 12:33:46 -0300

    Imagem e menu lateral iniciado

--- templates/padraogoverno01/index.php
+++ templates/padraogoverno01/index.php
@@ -29,7 +29,8 @@ $active_item = TmplPadraoGoverno01Helper::getActiveItemid();
     <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/bootstrap/css/bootstrap.min.css" type='text/css'/>
     <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/template-<?php echo $this->params->get('cor', 'verde'); ?>.css" type='text/css'/>
   <!-- CSS CUSTOMIZADO-->
-  <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/custom_css.css" type='text/css'/>
+  <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>css/template-verdev2.css" type='text/css'/>
+  <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/css/custom.css" type='text/css'/>
     <?php TmplPadraoGoverno01Helper::getIconsStyle( $this ); ?>
     <link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/font-awesome/css/font-awesome.min.css" type='text/css'/>
     <!--[if lt IE 10]>
