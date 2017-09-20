c473575663acbf8a80106c966c71096012013cb4
Thz069 <victor.thoma@gmail.com>
2017-09-20 13:20:45 -0300

    Reconstrução e limpeza dos plugins desnecessários

--- administrator/logs/error.php
+++
@@ -1,9 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2017-08-30 17:51:08 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2017-08-30T17:51:08+00:00	INFO ::1	joomlafailure	Não é permitido senha em branco
-2017-09-06T15:51:44+00:00	INFO ::1	joomlafailure	Senha inválida
-2017-09-06T18:12:50+00:00	INFO ::1	joomlafailure	Senha inválida
